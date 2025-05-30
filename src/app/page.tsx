4a5efdd90d597f55232d89f4b080ad405b610629
javonjax <javonj461@hotmail.com>
2025-05-29 20:07:22 -0400

    Updates to homepage layout.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,13 @@
-const HomePage = () => {
-  const val: number = Math.random() - 0.5;
-  return (
-    <div className="grid h-full w-full grid-cols-12 gap-6">
-      <div className="col-span-full flex flex-col items-center justify-center">
-        <h1 className="text-3xl">
-          {val > 0 ? 'Catch a wave. Find your sound.' : 'Tune in. Ride the waves.'}
-        </h1>
-        <h2>Browse radio stations from around the world, all in one place.</h2>
-      </div>
-      <div className="col-span-full flex items-center justify-center gap-6">
-        <a className="bg-button cursor-pointer rounded-xl border-2 p-4">Browse Stations</a>
-        <button className="rounded-xl border-2 p-4">FAQ</button>
-      </div>
-      <div className="col-span-full flex flex-col items-center gap-6">
-        <h2 className="text-heading mr-auto text-xl">Trending Now</h2>
-        <div className="flex h-[400px] w-full items-center justify-center border-2">Carousel</div>
-      </div>
-      <div className="col-span-full flex flex-col items-center gap-6">
-        <h2 className="text-heading mr-auto text-xl">Popular Station Tags</h2>
-        <div className="flex h-[400px] w-full items-center justify-center border-2">Carousel</div>
-      </div>
-    </div>
+import { Tag } from './api/lib/schemas';
+import HomePageContent from './components/HomePage/HomePageContent';
+
+const HomePage = async () => {
+  const res: globalThis.Response = await fetch(
+    'http://localhost:3000/api/tags?order=stationcount&reverse=true&hidebroken=true&limit=10'
   );
+  const tags: Tag[] = await res.json();
+
+  return <HomePageContent popularTags={tags} />;
 };
 
 export default HomePage;
