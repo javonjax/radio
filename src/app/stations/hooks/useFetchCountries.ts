dee38ab80711fea2673ed92e823229c2356a9f8c
javonjax <javonj461@hotmail.com>
2025-07-21 19:55:14 -0400

    Moved tanstack query logic into separate hooks for clarity.

---
+++ src/app/stations/hooks/useFetchCountries.ts
@@ -0,0 +1,20 @@
+import { Country } from '@/lib/api/schemas';
+import { handleAPIFetch } from '@/lib/utils';
+import { useQuery } from '@tanstack/react-query';
+
+export const useFetchCountries = () => {
+  const fetchCountries = async (): Promise<{ countries: Country[]; longestLabel: string }> => {
+    const url: string = `http://localhost:3000/api/countries`;
+    const res: globalThis.Response = await handleAPIFetch(await fetch(url));
+    const countries: Country[] = await res.json();
+    const longestLabel: string = countries.reduce((a, b) =>
+      a.name.length > b.name.length ? a : b
+    ).name;
+    return { countries, longestLabel };
+  };
+
+  return useQuery<{ countries: Country[]; longestLabel: string }>({
+    queryKey: ['fetchCountries'],
+    queryFn: fetchCountries,
+  });
+};
---
+++ src/app/stations/hooks/useFetchLanguages.ts
@@ -0,0 +1,20 @@
+import { Language } from '@/lib/api/schemas';
+import { handleAPIFetch } from '@/lib/utils';
+import { useQuery } from '@tanstack/react-query';
+
+export const useFetchLanguages = () => {
+  const fetchLanguages = async (): Promise<{ languages: Language[]; longestLabel: string }> => {
+    const url: string = `http://localhost:3000/api/languages`;
+    const res: globalThis.Response = await handleAPIFetch(await fetch(url));
+    const languages: Language[] = await res.json();
+    const longestLabel: string = languages.reduce((a, b) =>
+      a.name.length > b.name.length ? a : b
+    ).name;
+    return { languages, longestLabel };
+  };
+
+  return useQuery<{ languages: Language[]; longestLabel: string }>({
+    queryKey: ['fetchLanguages'],
+    queryFn: fetchLanguages,
+  });
+};
