ab0a2feb5eff3ae3a2f2a5af3dba592c0b3b9279
javonjax <javonj461@hotmail.com>
2025-07-21 20:01:32 -0400

    Updated player error handling. Minor updates to player controls.

--- src/components/Shared/Player/Player.tsx
+++ src/components/Shared/Player/Player.tsx
@@ -8,12 +8,15 @@ import PlayerControls from './PlayerControls';
 const Player = () => {
   const stationContext = useContext<StationContextType | undefined>(StationContext);
   const [isOpen, setIsOpen] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
   const audioRef = useRef<HTMLAudioElement>(null);
   const station: RadioStation | undefined = stationContext?.station;
 
   useEffect(() => {
     if (audioRef.current && stationContext) {
       audioRef.current.volume = stationContext.volume / 100;
+      setIsError(false);
     }
   }, [stationContext]);
 
@@ -43,14 +46,27 @@ const Player = () => {
     <>
       <PlayerControls
         isOpen={isOpen}
+        isLoading={isLoading}
+        isError={isError}
         setIsOpen={setIsOpen}
         stationContext={stationContext}
         handlePlay={handlePlay}
         handlePause={handlePause}
       />
+
       {station?.url_resolved ? (
         <>
-          <audio autoPlay ref={audioRef} src={station.url_resolved}></audio>
+          <audio
+            autoPlay
+            ref={audioRef}
+            src={station.url_resolved}
+            onLoadStart={() => setIsLoading(true)}
+            onCanPlay={() => setIsLoading(false)}
+            onError={() => {
+              setIsError(true);
+              setIsLoading(false);
+            }}
+          ></audio>
         </>
       ) : null}
     </>
--- src/components/Shared/Player/PlayerControls.tsx
+++ src/components/Shared/Player/PlayerControls.tsx
@@ -1,13 +1,16 @@
 import { StationContextType } from '@/components/ContextProviders/StationContext';
+import LoadingSpinner from '@/components/ui/Custom/LoadingSpinner';
 import { Slider } from '@/components/ui/slider';
 import { RadioStation } from '@/lib/api/schemas';
 import { capitalize } from '@/lib/utils';
-import { Heart, Info, Minus, Pause, Play, Plus, X } from 'lucide-react';
+import { Expand, Heart, Info, Minus, Pause, Play, X } from 'lucide-react';
 import { AnimatePresence, motion } from 'motion/react';
 
 export interface PlayerControlsProps {
   stationContext: StationContextType;
   isOpen: boolean;
+  isLoading: boolean;
+  isError: boolean;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
   handlePlay: () => void;
   handlePause: () => void;
@@ -16,40 +19,59 @@ export interface PlayerControlsProps {
 const PlayerControls = ({
   stationContext,
   isOpen,
+  isLoading,
+  isError,
   setIsOpen,
   handlePlay,
   handlePause,
 }: PlayerControlsProps) => {
   const station: RadioStation | undefined = stationContext?.station;
+
   return (
     <AnimatePresence>
-      {/* 
-        Full player when isOpen. Mini player otherwise.
-      */}
-      {isOpen ? (
-        <motion.div
-          layout
-          animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0, scale: 0.95 }}
-          transition={{
-            layout: { type: 'spring', stiffness: 300, damping: 25 },
-            duration: 0.1,
-          }}
-          className={`bg-background fixed right-[32px] bottom-[32px] flex w-[200px] min-w-[200px] flex-col gap-2 rounded-md border-2 p-4 ${stationContext?.station ? '' : 'hidden'}`}
-        >
-          <div className="flex items-center justify-between">
-            <p>Now playing</p>
-            <div className="flex items-center">
+      <motion.div
+        layout
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.95 }}
+        transition={{
+          layout: { type: 'spring', stiffness: 300, damping: 25 },
+          duration: 0.1,
+        }}
+        className={`bg-background fixed right-[32px] bottom-[32px] flex-col gap-2 rounded-md border-2 p-4 ${stationContext?.station ? '' : 'hidden'} ${isOpen ? 'flex w-[200px] min-w-[200px]' : 'max-w-[200px]'}`}
+      >
+        <>
+          {/* Minimize/maximize and close buttons. */}
+          {isOpen && !isError && (
+            <div className="flex items-center justify-between">
+              <p className={`${isError ? 'text-red-500' : ''}`}>
+                {isLoading ? 'Loading...' : isError ? 'Error' : 'Now playing'}
+              </p>
+              <div className="flex items-center">
+                <button
+                  className="cursor-pointer"
+                  onClick={() => {
+                    setIsOpen(false);
+                  }}
+                >
+                  <Minus />
+                </button>
+                <button
+                  className="cursor-pointer"
+                  onClick={() => {
+                    stationContext.setStation(undefined);
+                    stationContext.pause();
+                  }}
+                >
+                  <X className="text-red-500" />
+                </button>
+              </div>
+            </div>
+          )}
+          {isLoading && <LoadingSpinner />}
+          {isError && (
+            <div className="flex flex-col">
               <button
-                className="cursor-pointer"
-                onClick={() => {
-                  setIsOpen(false);
-                }}
-              >
-                <Minus />
-              </button>
-              <button
-                className="cursor-pointer"
+                className="cursor-pointer self-end"
                 onClick={() => {
                   stationContext.setStation(undefined);
                   stationContext.pause();
@@ -57,80 +79,71 @@ const PlayerControls = ({
               >
                 <X className="text-red-500" />
               </button>
+              <div className="text-red-500">
+                Unable to establish connection with {station?.name}
+              </div>
             </div>
-          </div>
-          <div className="flex flex-col gap-4">
-            {station?.url_resolved ? (
-              <>
-                <div className="w-full">
-                  {station.name && station.name.length > 0 ? (
-                    <p className="text-accent">{station.name}</p>
-                  ) : (
-                    <p className="text-accent">Name not found</p>
-                  )}
-                  {station.countrycode && (
-                    <p className="text-[12px]">Country: {station.countrycode}</p>
-                  )}
-                  {station.language && (
-                    <p className="text-[12px]">Language: {capitalize(station.language)}</p>
-                  )}
-                  {station.tags &&
-                    station.tags.length > 0 &&
-                    (() => {
-                      const tagString: string = station.tags
-                        ?.split(',')
-                        .slice(0, 2)
-                        .map((tag) => capitalize(tag))
-                        .join(', ');
-                      return <p className="text-[12px]">Tags: {tagString}</p>;
-                    })()}
-                </div>
-                <Slider
-                  className="bg-accent max-w-[200px] rounded-md"
-                  value={[stationContext.volume]}
-                  step={1}
-                  max={100}
-                  onValueChange={(value) => {
-                    stationContext.setVolume(value[0]);
-                  }}
-                />
-                <div className="flex items-center justify-center gap-4">
-                  <button
-                    className="w-fit cursor-pointer rounded-md p-2"
-                    onClick={stationContext.isPlaying ? handlePause : handlePlay}
-                  >
-                    {stationContext.isPlaying ? <Pause /> : <Play />}
-                  </button>
-                  <button
-                    style={{ backgroundImage: 'var(--accent-gradient)' }}
-                    className="w-fit cursor-pointer rounded-md p-2"
-                  >
-                    <Heart />
-                  </button>
-                  <button className="w-fit rounded-md p-2">
-                    <Info />
-                  </button>
-                </div>
-              </>
-            ) : (
-              <p className="w-full">Could not connect to {station?.name || ''}</p>
-            )}
-          </div>
-        </motion.div>
-      ) : (
-        /* Mini player.*/
-        <motion.div
-          layout
-          animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0, scale: 0.95 }}
-          transition={{
-            layout: { type: 'spring', stiffness: 300, damping: 25 },
-            duration: 0.1,
-          }}
-          className={`bg-background fixed right-[32px] bottom-[32px] flex flex-col gap-2 rounded-md border-2 p-4 ${stationContext?.station ? '' : 'hidden'}`}
-        >
-          {station?.url_resolved ? (
-            <div className="flex w-full justify-end gap-2">
+          )}
+          {/* Station info */}
+          {!isError && !isLoading && isOpen && (
+            <div className="flex flex-col gap-4">
+              {station?.url_resolved && (
+                <>
+                  <div className="w-full">
+                    {station.name && station.name.length > 0 ? (
+                      <p className="text-accent">{station.name}</p>
+                    ) : (
+                      <p className="text-accent">Name not found</p>
+                    )}
+                    {station.countrycode && (
+                      <p className="text-[12px]">Country: {station.countrycode}</p>
+                    )}
+                    {station.language && (
+                      <p className="text-[12px]">Language: {capitalize(station.language)}</p>
+                    )}
+                    {station.tags &&
+                      station.tags.length > 0 &&
+                      (() => {
+                        const tagString: string = station.tags
+                          ?.split(',')
+                          .slice(0, 2)
+                          .map((tag) => capitalize(tag))
+                          .join(', ');
+                        return <p className="text-[12px]">Tags: {tagString}</p>;
+                      })()}
+                  </div>
+                  <Slider
+                    className="bg-accent max-w-[200px] rounded-md"
+                    value={[stationContext.volume]}
+                    step={1}
+                    max={100}
+                    onValueChange={(value) => {
+                      stationContext.setVolume(value[0]);
+                    }}
+                  />
+                  <div className="flex items-center justify-center gap-4">
+                    <button
+                      className="w-fit cursor-pointer rounded-md p-2"
+                      onClick={stationContext.isPlaying ? handlePause : handlePlay}
+                    >
+                      {stationContext.isPlaying ? <Pause /> : <Play />}
+                    </button>
+                    <button
+                      style={{ backgroundImage: 'var(--accent-gradient)' }}
+                      className="w-fit cursor-pointer rounded-md p-2"
+                    >
+                      <Heart />
+                    </button>
+                    <button className="w-fit rounded-md p-2">
+                      <Info />
+                    </button>
+                  </div>
+                </>
+              )}
+            </div>
+          )}
+          {!isError && !isLoading && !isOpen && (
+            <div className="flex w-full gap-2">
               <button
                 className="w-fit cursor-pointer rounded-md"
                 onClick={stationContext.isPlaying ? handlePause : handlePlay}
@@ -143,7 +156,7 @@ const PlayerControls = ({
                   setIsOpen(true);
                 }}
               >
-                <Plus />
+                <Expand />
               </button>
               <button
                 className="cursor-pointer"
@@ -155,11 +168,9 @@ const PlayerControls = ({
                 <X className="text-red-500" />
               </button>
             </div>
-          ) : (
-            <p className="w-full">Could not connect to {station?.name || ''}</p>
           )}
-        </motion.div>
-      )}
+        </>
+      </motion.div>
     </AnimatePresence>
   );
 };
