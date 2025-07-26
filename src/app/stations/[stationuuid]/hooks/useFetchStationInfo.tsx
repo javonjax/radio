1e43277af92651cb519d220a6b042066eb89889b
javonjax <javonj461@hotmail.com>
2025-07-26 14:46:40 -0400

    Added basic station info page.

---
+++ src/app/stations/[stationuuid]/hooks/useFetchStationInfo.tsx
@@ -0,0 +1,22 @@
+import { RadioStation } from '@/lib/api/schemas';
+import { handleAPIFetch } from '@/lib/utils';
+import { useQuery } from '@tanstack/react-query';
+
+export const useFetchStationInfo = (stationuuid: string) => {
+  const fetchStation = async (): Promise<RadioStation> => {
+    /*
+        This API endpoint accepts a list of UUIDs as params and returns a list of stations.
+        This page only displays information for a single station so only the first list item is relevant.
+      */
+    const url: string = `http://localhost:3000/api/stations/search/byuuid?uuids=${stationuuid}`;
+    const res: globalThis.Response = await handleAPIFetch(await fetch(url));
+    const stations: RadioStation[] = await res.json();
+    return stations[0];
+  };
+
+  return useQuery<RadioStation>({
+    queryKey: ['fetchStation', stationuuid],
+    queryFn: fetchStation,
+    retry: false,
+  });
+};
